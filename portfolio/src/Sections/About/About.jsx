62d526207756a49f2316709ed3bdc1d0cd189e44
George Papoutsis <papoutsisgc@gmail.com>
2025-12-23 16:02:47 -0500

    Small changes

--- portfolio/src/Sections/About/About.jsx
+++ portfolio/src/Sections/About/About.jsx
@@ -15,8 +15,8 @@ export default function About({section}) {
                         I have worked with various programming languages such as
                         <span className="bold-secondary">&nbsp;Python, Java, JavaScript, C, and C++.&nbsp;</span> Throughout my past 4 years at Carleton University, I have had
                         the opportunity to learn and work with many Computer Engineering concepts including but not limited to <span className="bold-primary">Object-Oriented Programming, 
-                        Data Structures, Algorithms, Computer Architecture, Computer Communications, Network/ Software Security.</span> Some of the more ambitious projects I have worked on include, my 4th year Engineering Capstone
-                        Project titled "A Machine Learning Based Video Player for Streaming Applications", and a sorting algorithm visualizer using <span className="bold-secondary">HTML, CSS and JavaScript.&nbsp;</span> I have also seen 
+                        Data Structures, Algorithms, Computer Architecture, Computer Communications, and Network/ Software Security.</span> Some of the more ambitious projects I have worked on include, my 4th year Engineering Capstone
+                        Project titled "A Machine Learning Based Video Player for Streaming Applications", and a sorting algorithm visualizer using <span className="bold-secondary">HTML, CSS and JavaScript.&nbsp;</span> I have also experienced 
                         great success in my academic journey thus far. I have made the Dean's Honour List multiple years during the program, and I have been accepted in to the Accelerated Pathway Program which allows undergraduate students 
                         to take graduate level classes as part of the undergraduate degree.
                         <br /><br />I am a results-oriented self-starter with strong interpersonal skills who enjoys 
